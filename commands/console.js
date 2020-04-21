1a2fe94b52c1bc132633830e6edf44eaadf0b512
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2020-04-21 12:43:06 +0200

    qsfsdf

--- commands/console.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'console-clear',
-	description: 'Supprimer les messages de la console',
-	guildOnly: true,
-	execute(message, args) {
-        message.delete()
-        console.clear()
-	},
-};
