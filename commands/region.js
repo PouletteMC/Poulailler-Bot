9fd114cdedf0a762160831477ad1386393706171
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2020-11-07 04:01:52 +0100

    region

--- commands/region.js
+++ commands/region.js
@@ -1,21 +1,24 @@
 const id = require('../config.json');
 let admin = id.admin;
-
 module.exports = {
 	name: 'region',
 	description: 'Change la région',
 	guildOnly: true,
-	execute(message) {
+	execute(message, args) {
 
-	if (message.member.roles.cache.has(admin)){
-		const args = message.content.slice(prefix.length).trim().split(' ');
-		const region = args.shift().toLowerCase();
+		if (!args.length) {
 
-        message.delete().catch(O_o=>{})
+			return message.channel.send("Cette région n'éxiste pas. Les régions valides sont: amsterdam, india, southafrica, london, us-east, us-west, us-central, south-korea, russia, eu-central, sydney, brazil, eu-west, europ, us-south, hongkong, singapour, frankfurt, dubai, japan")
+
+		}
+
+		else {const region = args.shift().toLowerCase()
+
+		message.delete().catch(O_o=>{})
+		console.clear()
         message.guild.setRegion(region)
-			.then(console.log("L'allocation du serveur a changé !"), message.channel.send('Le serveur a été déplacé.'))
+			.then(console.log("L'allocation du serveur a changé !"), message.channel.send('Le serveur a été déplacé en ' + region))
 			.catch(console.error);
 		} 
-	else { console.log('it broke my dude')}
 	},
 };
