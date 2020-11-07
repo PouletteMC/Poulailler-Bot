060321732cdb134d08acb23bda0b770897991e85
PouletteMC <63922760+PouletteMC@users.noreply.github.com>
2020-11-07 15:40:26 +0100

    Added region command

--- commands/region.js
+++ commands/region.js
@@ -1,31 +1,48 @@
 const id = require('../config.json');
 let admin = id.admin;
+
 module.exports = {
-	name: 'region',
-	description: 'Change la région',
-	guildOnly: true,
-	execute(message, args) {
+    name: 'region',
+    description: 'Change la région',
+    guildOnly: true,
+    execute(message, args) {
 
-		const region = args.shift().toLowerCase()
+        if (!args.length) {
 
-		if (!args.length) {
+            return message.channel.send("Cette région n'éxiste pas. Les régions valides sont: amsterdam, india, southafrica, london, us-east, us-west, us-central, south-korea, russia, eu-central, sydney, brazil, eu-west, europ, us-south, hongkong, singapour, frankfurt, dubai, japan")
 
-			return message.channel.send("Cette région n'éxiste pas. Les régions valides sont: amsterdam, india, southafrica, london, us-east, us-west, us-central, south-korea, russia, eu-central, sydney, brazil, eu-west, europ, us-south, hongkong, singapour, frankfurt, dubai, japan")
+        }
 
-		}
+        else {
+            const region = message.content.slice(7).trim().split(' ').shift().toLowerCase()
+            switch(region) {
 
-		if (args[1] === 'cycle') {
-			message.delete().catch(O_o=>{})
-			message.guil.setRegion('london')
-			.then(message.guild.setRegion('eu-west'))
-			.then(console.log("Cycle de région terminé !"), message.channel.send("Cycle terminé, si le problème persiste vérifies sur https://discordstatus.com/ "))
-		}
+                case 'europe':
+                    message.member.guild.setRegion('europe')
+                    message.channel.send('Le serveur a été déplacé vers la région **Europe**')
+                    break;
 
-		else {
-			message.delete().catch(O_o=>{})
-        	message.guild.setRegion(region)
-			.then(console.log("L'allocation du serveur a changé !"), message.channel.send('Le serveur a été déplacé en ${region}'))
-			.catch(console.error);
-		} 
-	},
+                case 'london':
+                    message.member.guild.setRegion('london')
+                    message.channel.send('Le serveur a été déplacé vers la région **London**')
+                    break;
+
+                case 'eu-west':
+                    message.member.guild.setRegion('eu-west')
+                    message.channel.send('Le serveur a été déplacé vers la région **eu-west**')
+                    break;
+
+                case 'cycle':
+                    message.member.guild.setRegion('south-korea')
+                    message.member.guild.setRegion('europe')
+                    message.channel.send("L'allocation du serveur a changé, si il y a encore des problèmes vérifies https://discordstatus.com/")
+                    break;
+
+                default:
+                    message.channel.send("Cette région n'éxiste pas.")
+                
+
+            }
+        }
+    },
 };
