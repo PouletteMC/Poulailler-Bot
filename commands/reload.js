0d8c02a40776b39fb2d6ce16bd37007bc2bc6430
Poulette <63922760+PouletteMC@users.noreply.github.com>
2020-11-18 10:02:41 +0100

    fixes

--- commands/reload.js
+++ commands/reload.js
@@ -4,8 +4,11 @@ module.exports = {
 	name: 'reload',
 	description: 'Reloads a command',
 	execute(message, args) {
+
 		if(message.member.roles.cache.has(admin)){
-        if (!args.length) return message.channel.send(`${message.author} tu n'as pas précisé la commande à recharger !`);
+
+		if (!args.length) return message.channel.send(`${message.author} tu n'as pas précisé la commande à recharger !`);
+		
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 		|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
@@ -17,11 +20,15 @@ if (!command)
 try {
 	const newCommand = require(`./${command.name}.js`);
 	message.client.commands.set(newCommand.name, newCommand);
+	message.channel.send(`La commande \`${command.name}\` a été rechargée!`);
+
+
 } catch (error) {
+
 	console.log(error);
-    message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
-	message.channel.send(`Command \`${command.name}\` was reloaded!`);
+    message.channel.send(`Une erreur est survenue pendant le reload de  \`${command.name}\`:\n\`${error.message}\``);
 			}
+
 		}else{
 			message.delete().catch(O_o=>{});
 			message.reply('Cette commande est réservés aux Admins.')
