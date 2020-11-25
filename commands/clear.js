1e393678c421b141cb60bc376e14353129b923af
Poulette <63922760+PouletteMC@users.noreply.github.com>
2020-11-25 11:31:28 +0100

    New clear command

--- commands/clear.js
+++ commands/clear.js
@@ -9,22 +9,25 @@ module.exports = {
 	guildOnly: true,
 	execute(message, args) {
 
-        const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
-    const amount = args.join(' '); // Amount of messages which should be deleted
+        const arguments = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+    const amount = arguments.join(' '); // Amount of messages which should be deleted
 
     if (!amount)
-        return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
+        return message.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
 
     if (isNaN(amount))
-        return msg.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+        return message.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
 
     if (amount > 100)
-        return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
+        return message.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
 
     if (amount < 1)
-        return msg.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
+        return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 
-    await msg.channel.messages.fetch({ limit: amount }).then(messages => {msg.channel.bulkDelete(messages)});
+    else {
+        message.channel.bulkDelete(amount)
+        console.log(`J'ai supprimé ${amount} messages`)
+    }
 
     }
 }
