88da68222a4dc988a5b37a8b36ff659c525f9869
Poulette <63922760+PouletteMC@users.noreply.github.com>
2020-11-25 11:19:28 +0100

    revamped clear command

--- commands/clear.js
+++ commands/clear.js
@@ -7,16 +7,24 @@ module.exports = {
 	aliases: ['cls'],
 	description: 'Clear !',
 	guildOnly: true,
-	execute(message) {
+	execute(message, args) {
 
-		if(message.member.roles.cache.has(admin)){
-			message.channel.bulkDelete(cls).catch(O_o=>{});
-			console.log('Les messages ont été supprimés')
-		}
-		else {
-			message.delete().catch(O_o=>{});
-			message.reply('Cette commande est réservés aux Admins.')
-			console.log(`${message.author.username} a tenté d'utiliser la commande clear`)
-	  }
-	},
-};
+        const args = message.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+    const amount = args.join(' '); // Amount of messages which should be deleted
+
+    if (!amount)
+        return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
+
+    if (isNaN(amount))
+        return msg.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+
+    if (amount > 100)
+        return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
+
+    if (amount < 1)
+        return msg.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
+
+    await msg.channel.messages.fetch({ limit: amount }).then(messages => {msg.channel.bulkDelete(messages)});
+
+    }
+}
